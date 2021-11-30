08f48256a56141d5a6df277ea9a47f4b68aa25be
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-30 17:17:57 +0200

    adding

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const notFound = require("../src/errors/404");
 const PORT = process.env.PORT || 3500;
 const uuid = require('uuid').v4;
 
+
 // server.use(cors());
 server.use(cors({ origin: '*' }));
 server.use(morgan("dev"));
