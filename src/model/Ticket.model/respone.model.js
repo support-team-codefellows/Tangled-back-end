813872993678b1135b7ab4f6e630a2edaf17731d
Marwan Amir zakia <84706279+Marwan-Zakia@users.noreply.github.com>
2022-01-02 20:52:42 +0200

    db

--- src/model/Ticket.model/respone.model.js
+++ src/model/Ticket.model/respone.model.js
@@ -2,7 +2,8 @@ const response = (sequelize, DataTypes) =>
   sequelize.define("response", {
     username: { type: DataTypes.STRING, required: true },
     response: { type: DataTypes.STRING, required: true },
-    
+    customername: { type: DataTypes.STRING, required: true },
+    ticketid: {type: DataTypes.INTEGER} 
   });
 
 module.exports = response;
--- src/model/index.js
+++ src/model/index.js
@@ -1,57 +1,67 @@
 'use strict'
-const {Sequelize,DataTypes}=require('sequelize');
-const Users=require('./users.model/users.model');
-const Collection=require('./collection');
-const Ticket=require('./Ticket.model/Ticket')
-const OnsiteTicket=require('./Ticket.model/onSite.model')
-const response=require('./Ticket.model/respone.model')
+const { Sequelize, DataTypes } = require('sequelize');
+const Users = require('./users.model/users.model');
+const Collection = require('./collection');
+const Ticket = require('./Ticket.model/Ticket')
+const OnsiteTicket = require('./Ticket.model/onSite.model')
+const response = require('./Ticket.model/respone.model')
 
 
-const POSTGRES_URL= process.env.NODE_ENV ==='test'? "sqlite:memory:" : process.env.DATABASE_URL
+const POSTGRES_URL = process.env.NODE_ENV === 'test' ? "sqlite:memory:" : process.env.DATABASE_URL
 
 let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     }
-  } : {};
+  }
+} : {};
 
 
-  let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
+let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
-  const usersModel=Users(sequelize,DataTypes)
-  const usersCollection= new Collection(usersModel)
-  
-  const ticketModel=Ticket(sequelize,DataTypes)
-  const ticketCollection=new Collection(ticketModel)
+const usersModel = Users(sequelize, DataTypes)
+const usersCollection = new Collection(usersModel)
 
-  const OnsiteTicketModel=OnsiteTicket(sequelize,DataTypes)
-  const OnsiteTicketCollection=new Collection(OnsiteTicketModel)
+const ticketModel = Ticket(sequelize, DataTypes)
+const ticketCollection = new Collection(ticketModel)
 
-  const responseModel=response(sequelize,DataTypes)
-  const responseCollection=new Collection(responseModel)
-    
-    module.exports ={
-      
-        db: sequelize,
-        usersCollection:usersCollection,
-        Users: usersModel,
+const OnsiteTicketModel = OnsiteTicket(sequelize, DataTypes)
+const OnsiteTicketCollection = new Collection(OnsiteTicketModel)
 
-        ticketCollection: ticketCollection,
-        Ticket: ticketModel,
+const responseModel = response(sequelize, DataTypes)
+const responseCollection = new Collection(responseModel)
 
-        OnsiteTicketCollection:OnsiteTicketCollection,
-        OnsiteTicket:OnsiteTicketModel,
+///========================
+ticketModel.hasMany(responseModel, { foreignKey: 'ticketid', sourceKey: 'id' });
+responseModel.belongsTo(ticketModel, { foreignKey: 'ticketid', targetKey: 'id' });
 
-        responseCollection:responseCollection,
-        responseModel:responseModel
+///========================
+
+OnsiteTicketModel.hasMany(responseModel, { foreignKey: 'ticketid', sourceKey: 'id' });
+responseModel.belongsTo(OnsiteTicketModel, { foreignKey: 'ticketid', targetKey: 'id' });
 
 
-    }
+module.exports = {
 
-  
+  db: sequelize,
+  usersCollection: usersCollection,
+  Users: usersModel,
+
+  ticketCollection: ticketCollection,
+  Ticket: ticketModel,
+
+  OnsiteTicketCollection: OnsiteTicketCollection,
+  OnsiteTicket: OnsiteTicketModel,
+
+  responseCollection: responseCollection,
+  responseModel: responseModel
+
+
+}
+
+
 
 
 
